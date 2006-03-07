792fde0d169230910f1f62cb25071aee1d1b4342
m0gg <m0gg>
2006-03-07 11:32:42 +0000

    File header changed

--- nanohttp/nanohttp-ssl.c
+++ nanohttp/nanohttp-ssl.c
@@ -1,29 +1,26 @@
-/*************************************************************************
- * $Id: nanohttp-ssl.c,v 1.18 2006/03/06 13:37:38 m0gg Exp $
- *
- * CSOAP Project: A http client/server library in C
- * Copyright (C) 2001-2005  Rochester Institute of Technology
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
-/*
- * Author:		Matt Campbell
- * Descrip:		SSL connection routines
- */
-
+/******************************************************************
+*  $Id: nanohttp-ssl.c,v 1.19 2006/03/07 11:32:42 m0gg Exp $
+*
+* CSOAP Project:  A http client/server library in C
+* Copyright (C) 2001-2005  Rochester Institute of Technology
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the
+* Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+* Boston, MA  02111-1307, USA.
+*
+* Author: Matt Campbell
+******************************************************************/
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
--- nanohttp/nanohttp-ssl.h
+++ nanohttp/nanohttp-ssl.h
@@ -1,24 +1,26 @@
-/*
- * Copyright (C) 2001-2005  Rochester Institute of Technology
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
-/*
- * Author:		Matt Campbell
- */
+/******************************************************************
+*  $Id: nanohttp-ssl.h,v 1.11 2006/03/07 11:32:42 m0gg Exp $
+*
+* CSOAP Project:  A http client/server library in C
+* Copyright (C) 2001-2005  Rochester Institute of Technology
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the
+* Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+* Boston, MA  02111-1307, USA.
+*
+* Author: Matt Campbell
+******************************************************************/
 #ifndef __NANOHTTP_SSL_H_
 #define __NANOHTTP_SSL_H_
 
