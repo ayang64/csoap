c2a1e6e2626b7c37bcbbf8748bb6c21fa0c31ef3
m0gg <m0gg>
2007-01-01 23:23:16 +0000

    Documentation enhancements

--- nanohttp/nanohttp-mime.h
+++ nanohttp/nanohttp-mime.h
@@ -3,7 +3,7 @@
 * | \/ | | | | \/ | | _/
 * |_''_| |_| |_''_| |_'/  PARSER
 *
-*  $Id: nanohttp-mime.h,v 1.15 2007/01/01 18:58:05 m0gg Exp $
+*  $Id: nanohttp-mime.h,v 1.16 2007/01/01 23:23:16 m0gg Exp $
 *
 * CSOAP Project:  A http client/server library in C
 * Copyright (C) 2003-2004  Ferhat Ayaz
@@ -56,22 +56,25 @@
  * httpc_mime_begin(conn, url, start_id, "", "text/tml")
  * httpc_mime_next(conn, start_id, "text/xml", "binary")
  * http_output_stream_write(conn->out, buffer, size)
- * http_mime_send_file()
- * http_mime_end(conn, &res)
+ * httpc_mime_send_file()
+ * httpc_mime_end(conn, &res)
  * @endcode
  *
  * @subsection nanohttp_mime_client_response_sec Read the server response
  *
  * @subsection nanohttp_mime_client_cleanup_sec Client cleanup
  *
- * @secion nanohttp_mime_server_sec nanoHTTP server with MIME attachments
+ * See @ref nanohttp_client_cleanup_sec for more information about client
+ * cleanup.
+ *
+ * @section nanohttp_mime_server_sec nanoHTTP server with MIME attachments
  *
  * @subsection nanohttp_mime_server_init_sec Server initialization
  *
  * @subsection nanohttp_mime_server_cleanup_sec Server cleanup
  *
  * @author	Ferhat Ayaz
- * @version	$Revision: 1.15 $
+ * @version	$Revision: 1.16 $
  *
  * @see		http://www.ietf.org/rfc/rfc2045.txt
  * @see		http://www.ietf.org/rfc/rfc2046.txt
